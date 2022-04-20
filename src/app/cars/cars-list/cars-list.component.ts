fa21eaf27c7cd4f77edd7a22f334a1a504cb67b2
Hubert_Rosniak <hubertcfc@gmail.com>
2022-04-20 22:25:32 +0200

    EK-1

---
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from '../models/car';
+
+@Component({
+  selector: 'cs-cars-list',
+  templateUrl: './cars-list.component.html',
+  styleUrls: ['./cars-list.component.less']
+})
+export class CarsListComponent implements OnInit {
+
+  cars: Car[] = [
+    {
+      id: 1,
+      model: 'Mazda Rx7',
+      plate: 'GD2121E',
+      deliveryDate: '21-04-2017',
+      deadline: '05-05-2016',
+      client: {
+        firstName: 'Jan',
+        surname: 'Kowalski'
+      },
+      cost: 300,
+      isFullyDamaged: true
+    },
+    {
+      id: 2,
+      model: 'Mercedes 124',
+      plate: 'KRK2200',
+      deliveryDate: '24-05-2017',
+      deadline: '03-06-2016',
+      client: {
+        firstName: 'Micha�',
+        surname: 'Nowak'
+      },
+      cost: 1200,
+      isFullyDamaged: true
+    },
+    {
+      id: 3,
+      model: 'Renault CLIO',
+      plate: 'GWE22011',
+      deliveryDate: '02-02-2017',
+      deadline: '03-03-2017',
+      client: {
+        firstName: 'Beata',
+        surname: 'Dampc'
+      },
+      cost: 2800,
+      isFullyDamaged: true
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/cars/cars.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarsListComponent } from './cars-list/cars-list.component';
+
+
+
+@NgModule({
+  exports: [CarsListComponent],
+  declarations: [
+    CarsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarsModule { }
